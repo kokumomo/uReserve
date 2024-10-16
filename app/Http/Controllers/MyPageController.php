2f2d404af999f1734f5568bafc1a5e5a77d89183
{km} <{k3042227625n@gmail.com}>
2024-10-17 07:07:46 +0900

    114-118

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Event;
+use App\Models\Reservation;
 use Illuminate\Support\Facades\Auth;
 use App\Services\MyPageService;
+use Carbon\Carbon;
 
 class MyPageController extends Controller
 {
@@ -16,9 +19,34 @@ class MyPageController extends Controller
         $fromTodayEvents = MyPageService::reservedEvent($events, 'fromToday');
         $pastEvents = MyPageService::reservedEvent($events, 'past');
 
-        // dd($user, $events, $fromTodayEvents, $pastEvents);
         return view('mypage/index', 
         compact('fromTodayEvents', 'pastEvents'));
     }
 
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservation = Reservation::where('user_id', '=', Auth::id())
+        ->where('event_id', '=', $id)
+        ->latest()
+        ->first();
+
+        return view('mypage/show', compact('event', 'reservation'));
+
+    }
+
+    public function cancel($id)
+    {
+        $reservation = Reservation::where('user_id', '=', Auth::id())
+        ->where('event_id', '=', $id)
+        ->latest()
+        ->first();
+
+        $reservation->canceled_date = Carbon::now()->format('Y-m-d H:i:s');
+        $reservation->save();
+
+        session()->flash('status', 'キャンセルできました');
+
+        return to_route('dashboard');
+    }
 }
--- resources/views/layouts/calendar.blade.php
+++ resources/views/layouts/calendar.blade.php
@@ -1,41 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js'])
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        @livewireStyles
-        
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js'])
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+    <!-- Styles -->
+    @livewireStyles
+
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
             @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @endauth
         </div>
-        <script src="{{ mix('js/flatpickr.js')}}"></script>
+        @endif
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
 
-        @livewireScripts
-    </body>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    <script src="{{ mix('js/flatpickr.js')}}"></script>
+
+    @livewireScripts
+</body>
+
 </html>
