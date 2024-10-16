9f8c7545d8b5d0b9bd112ca1aa2ab69987915c61
{km} <{k3042227625n@gmail.com}>
2024-10-17 06:07:53 +0900

    108-113

---
+++ app/Http/Controllers/MyPageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Services\MyPageService;
+
+class MyPageController extends Controller
+{
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $events = $user->events;
+        $fromTodayEvents = MyPageService::reservedEvent($events, 'fromToday');
+        $pastEvents = MyPageService::reservedEvent($events, 'past');
+
+        // dd($user, $events, $fromTodayEvents, $pastEvents);
+        return view('mypage/index', 
+        compact('fromTodayEvents', 'pastEvents'));
+    }
+
+}
